2224c12d83ed414ba1baa5c331008c9519449ad9
Aishwarya Dhagade <dhagadeaishwarya@gmail.com>
2026-05-07 23:59:11 +0530

    initial backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+    
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
